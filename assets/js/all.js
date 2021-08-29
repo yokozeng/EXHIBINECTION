39c2f85e5a27ca7f0cbc947209c692c95d3353c2
yoko <yokozeng227@gmail.com>
2021-08-29 20:30:42 +0800

    Update 2021-08-29T12:30:36.035Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,14 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.75,
+      spaceBetween: 30
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
